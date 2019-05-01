dc13b091be8c16313ebb7f667a4e6333edaf4a27
ihbaraba <ihbaraba@gmail.com>
2019-05-01 15:33:53 +0300

    fix search products

--- src/containers/AdministratorSide/ContractorProducts/ContractorProducts.js
+++ src/containers/AdministratorSide/ContractorProducts/ContractorProducts.js
@@ -43,7 +43,7 @@ class ContractorProducts extends Component {
         const {currentPage, filters: {category_id, name, brand, in_stock, vendor_code, min_price, max_price}} = this.state;
         const urlParams = [
             category_id ? `&category_id=${category_id}` : '',
-            name ? `&name__contains=${name}` : '',
+            name ? `&name=${name}` : '',
             brand ? `&brand=${brand}` : '',
             in_stock ? `&in_stock=${in_stock}` : '',
             vendor_code ? `&vendor_code=${vendor_code}` : '',

d83bf59647c5951b7d9d2bb6e01ea02d90944d1c
Vadym Hevlich <vege1wgw@gmail.com>
2019-05-28 17:06:24 +0300

    price display fixed

--- src/containers/AdministratorSide/Categories/Categories.js
+++ src/containers/AdministratorSide/Categories/Categories.js
@@ -245,7 +245,7 @@ class Categories extends Component {
                                 type="number"
                                 className={styles.productName}
                                 name='min_price'
-                                value={min_price}
+                                value={min_price * 1.05}
                                 onChange={this.handleChangeFilters}
                             />
                         </div>
@@ -255,7 +255,7 @@ class Categories extends Component {
                                 type="number"
                                 className={styles.productName}
                                 name='max_price'
-                                value={max_price}
+                                value={max_price * 1.05}
                                 onChange={this.handleChangeFilters}
                             />
                         </div>

8aa39859bbdb2e3dca378b34915b273bfc099c41
Pecherskiy88 <pecherskiy88@gmail.com>
2019-06-11 16:54:34 +0300

    change apiurls

--- src/constants/APIURLS.js
+++ src/constants/APIURLS.js
@@ -1,24 +1,26 @@
 const SERVERS = {
-    PRODUCT: {
-        XHR: "https://api.smartlead.top/api/v1/",
-    },
-    DEV: {
-        XHR: "https://api.smartlead.top/api/v1/",
-    },
+  PRODUCT: {
+    // XHR: "https://api.smartlead.top/api/v1/",
+    XHR: "https://api.topmarket.ua/api/v1/"
+  },
+  DEV: {
+    XHR: "https://api.topmarket.ua/api/v1/"
+  }
 };
 
 /*
-* Getting current URL.
-* Aiming to avoid wrong backend server address
-* If it is not "localhost" setting SERVERS.PRODUCT
-* */
+ * Getting current URL.
+ * Aiming to avoid wrong backend server address
+ * If it is not "localhost" setting SERVERS.PRODUCT
+ * */
 
 const generatedDetected = {
-    XHR: `https://api.${document.location.hostname}/api/v1/`,
-    SOCKET: `wss://api.${document.location.hostname}/`,
+  XHR: `https://api.${document.location.hostname}/api/v1/`,
+  SOCKET: `wss://api.${document.location.hostname}/`
 };
 
-export const BASE_URL = document.location.hostname === "localhost"
+export const BASE_URL =
+  document.location.hostname === "localhost"
     ? SERVERS.DEV.XHR /* <=== set here server what needs for developing -  */
     : generatedDetected.XHR;
 
@@ -26,79 +28,72 @@ export const BASE_URL = document.location.hostname === "localhost"
  * */
 
 //USER
-export const LOGIN = 'login/';
+export const LOGIN = "login/";
 
-export const REGISTRATION = 'register/';
+export const REGISTRATION = "register/";
 
-export const CONFIRM_EMAIL = 'activate/';
+export const CONFIRM_EMAIL = "activate/";
 
-export const PROFILE = 'profile/';
+export const PROFILE = "profile/";
 
-export const PASSWORD = 'password_change/';
+export const PASSWORD = "password_change/";
 
-export const RESET_PASSWORD = 'password_reset/';
+export const RESET_PASSWORD = "password_reset/";
 
-export const CONTACT_FORM = 'marketplace/base/contact_us/';
+export const CONTACT_FORM = "marketplace/base/contact_us/";
 
-export const INVOICE = 'marketplace/base/liqpay/';
+export const INVOICE = "marketplace/base/liqpay/";
 
 //COMPANY
-export const COMPANY_PROFILE = 'company/detail/';
+export const COMPANY_PROFILE = "company/detail/";
 
-export const UPDATE_COMPANY_PROFILE = 'company/update/';
+export const UPDATE_COMPANY_PROFILE = "company/update/";
 
-export const GET_COMPANY_TYPE = 'company/company_type/';
+export const GET_COMPANY_TYPE = "company/company_type/";
 
-export const GET_ACTIVITY_AREAS = 'company/activity_areas/';
+export const GET_ACTIVITY_AREAS = "company/activity_areas/";
 
-export const GET_SERVICE_INDUSTRY = 'company/service_industry/';
+export const GET_SERVICE_INDUSTRY = "company/service_industry/";
 
-export const COMPANY_DOCUMENTS = 'company/documents/';
+export const COMPANY_DOCUMENTS = "company/documents/";
 
-export const COMPANY_PITCH = 'company/pitch/';
+export const COMPANY_PITCH = "company/pitch/";
 
 //PRODUCTS
-export const CONTRACTOR_PRODUCTS = 'catalog/contractor_products/';
+export const CONTRACTOR_PRODUCTS = "catalog/contractor_products/";
 
-export const PARTNER_PRODUCTS = 'catalog/partner_products/';
+export const PARTNER_PRODUCTS = "catalog/partner_products/";
 
-export const ALL_PRODUCTS = 'catalog/partner_products/products_by_contractors/';
+export const ALL_PRODUCTS = "catalog/partner_products/products_by_contractors/";
 
-export const CONTRACTOR_CATEGORIES = 'catalog/contractor_products/contractor_categories/';
+export const CONTRACTOR_CATEGORIES =
+  "catalog/contractor_products/contractor_categories/";
 
-export const UPLOAD_PRODUCTS = 'catalog/products_upload/';
+export const UPLOAD_PRODUCTS = "catalog/products_upload/";
 
-export const REMOVE_CONTRACTOR_PRODUCTS = 'catalog/contractor_products/delete_list_of_products/';
+export const REMOVE_CONTRACTOR_PRODUCTS =
+  "catalog/contractor_products/delete_list_of_products/";
 
-export const NEW_PRODUCTS = 'catalog/contractor_products/';
+export const NEW_PRODUCTS = "catalog/contractor_products/";
 
-export const DOWNLOADS_STATUS = 'catalog/contractor_products/upload_history/';
+export const DOWNLOADS_STATUS = "catalog/contractor_products/upload_history/";
 
-export const COPY_TO_MY_PRODUCTS = 'catalog/partner_products/copy_to_my_products/';
+export const COPY_TO_MY_PRODUCTS =
+  "catalog/partner_products/copy_to_my_products/";
 
-export const YML = 'catalog/yml-handler/';
-
-
+export const YML = "catalog/yml-handler/";
 
 //CATEGORIES
-export const ALL_CATEGORIES = 'catalog/categories/';
+export const ALL_CATEGORIES = "catalog/categories/";
 
-export const FIRST_LEVEL_CATEGORIES = 'catalog/categories/first_level/';
-
+export const FIRST_LEVEL_CATEGORIES = "catalog/categories/first_level/";
 
 //ORDERS
-export const ORDERS = 'orders/';
+export const ORDERS = "orders/";
 
-export const PASS_TO_CONTRACTOR = (id) => `orders/${id}/pass_to_contractor/`;
+export const PASS_TO_CONTRACTOR = id => `orders/${id}/pass_to_contractor/`;
 
-export const CONTRACTOR_ORDERS = 'orders_contractor/';
+export const CONTRACTOR_ORDERS = "orders_contractor/";
 
 //STORE
-export const STORE = 'my_store/';
-
-
-
-
-
-
-
+export const STORE = "my_store/";
